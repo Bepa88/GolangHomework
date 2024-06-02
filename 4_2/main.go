f07593a1bec855c31a2eda18c43ecb6541d44562
Bepa88 <leleiko.vira@gmail.com>
2024-06-02 18:52:54 +0200

    Update homework 4

--- 4_2/main.go
+++ 4_2/main.go
@@ -11,13 +11,14 @@ type MyStruct struct {
 
 func UniqueData(slice []MyStruct) []MyStruct {
 	sort.Slice(slice, func(i, j int) bool { return slice[i].ID < slice[j].ID })
+	j := 0
 	for i := 1; i < len(slice); i++ {
-		if slice[i].ID == slice[i-1].ID {
-			slice = append(slice[:i-1], slice[i:]...)
-			i--
+		if slice[j].ID != slice[i].ID {
+			j++
+			slice[j] = slice[i]
 		}
 	}
-	return slice
+	return slice[:j+1]
 }
 
 func main() {
